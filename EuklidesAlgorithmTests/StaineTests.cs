ed2fb0877f6c7911e593fc9e569a176133823457
Вадим Денисовский <vadim.denisovski@live.com>
2019-11-05 23:07:22 +0300

    Unit - тест для алгаритма Стейна

---
+++ EuklidesAlgorithmTests/StaineTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Euklides.Tests
+{
+    /// <summary>
+    /// Summary description for StaineTests
+    /// </summary>
+    [TestClass]
+    public class StaineTests
+    {        
+        [TestMethod]
+        public void GetBiGCD_48and36_12returned()
+        {
+            // arrange
+            int a = 48;
+            int b = 36;
+            int expeted = 12;
+            // action
+            Staine st = new Staine();
+            int actual = st.GetBiGCD(a, b);
+            // assert
+            Assert.AreEqual(expeted, actual);
+        }
+    }
+}
