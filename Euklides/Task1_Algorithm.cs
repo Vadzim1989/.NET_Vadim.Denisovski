d82c9ec83d9636714347fd41cf7f062f7dff781f
Вадим Денисовский <vadim.denisovski@live.com>
2019-11-07 01:32:09 +0300

    Все алгоритмы реализованы в классе task1_algorithm. Добавлен метод подготавливающий данные для гистограммы. В EuklidesTest добавлены тест для нахождения НОД: 3-ех чисел; 4-ех чисел и 5-ти чисел.

---
+++ Euklides/Task1_Algorithm.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Euklides
+{
+    public class Task1_Algorithm
+    {
+        /// <summary> Реализация алгоритма Евклида 
+        /// </summary>
+        /// Для вычесления НОД используем цикл while
+        /// в цикле задаем условие (b не равно 0)
+        /// производим математические вычисления              
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>
+        /// Наибольший Общий Делитель (НОД)
+        /// </returns>
+        /// 
+
+        public int GetGCD(int a, int b) // НОД 2-ух целых чисел. Алгоритм Евклида
+        {
+            while (b != 0)
+                b = a % (a = b);
+            return a;
+        }
+
+        public int GetGCD(int a, int b, int c) // НОД 3-ех целых чисел. Алгоритм Евклида
+        {
+            return GetGCD(GetGCD(a, b), c);
+        }
+
+        public int GetGCD(int a, int b, int c, int d) // НОД 4-ех целых чисел. Алгоритм Евклида
+        {
+            return GetGCD(GetGCD(GetGCD(a, b), c), d);
+        }
+
+        public int GetGCD(int a, int b, int c, int d, int e) // НОД 5-ти целых чисел. Алгоритм Евклида
+        {
+            return GetGCD(GetGCD(GetGCD(GetGCD(a, b), c), d), e);
+        }
+
+        /// <summary>
+        /// Алгоритм Евклида с выходным параметром 
+        /// </summary>
+        /// В данном алгоритме принимается выходной параметр
+        /// Выходным параметром является время потраченое на решение.
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="euklidTime"></param>
+        /// <returns>
+        /// Наибольший Общий Делитель (НОД)
+        /// Время затраченное на решение данного алгоритма
+        /// </returns>
+        public int GetGCD(int a, int b, out long euklidTime) // НОД 2-ух целых чисел с выходным параметром времени. Алгоритм Евклида
+        {
+            Stopwatch st = new Stopwatch();
+            st.Start();
+            while (b != 0)
+                b = a % (a = b);
+            st.Stop();
+            euklidTime = st.ElapsedMilliseconds;
+            return a;
+        }
+
+        /// <summary>
+        /// Реализация алгоритма Стейна
+        /// </summary>
+        /// В данном методе описываем реализацию алгоритма Стейна
+        /// В методе присутсвует выходной параметр времени потраченое на решение.
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>
+        /// Наибольший Общий Делитель (НОД)
+        /// Время затраченное на решение данного алгоритма
+        /// </returns>
+        public int GetBiGCD(int a, int b, out long staineTime) // НОД 2-ух чисел с выходным параметром времени. Алгоритм Стейна
+        {
+            long time;
+            int result;
+            Stopwatch st = new Stopwatch();
+            st.Start();
+
+            if (a == 0) { st.Stop(); staineTime = st.ElapsedMilliseconds; return b; }
+            if (b == 0) { st.Stop(); staineTime = st.ElapsedMilliseconds; return a; }
+            if (a == b) { st.Stop(); staineTime = st.ElapsedMilliseconds; return a; }
+            if (a == 1 || b == 1) { st.Stop(); staineTime = st.ElapsedMilliseconds; return 1; }
+            if ((a % 2 == 0) && (b % 2 == 0))
+            {
+                result = 2 * GetBiGCD(a / 2, b / 2, out time);
+                st.Stop();
+                staineTime = st.ElapsedMilliseconds + time;
+                return result;
+            }
+            if ((a % 2 == 0) && (b % 2 != 0))
+            {
+                result = GetBiGCD(a / 2, b, out time);
+                st.Stop();
+                staineTime = st.ElapsedMilliseconds + time;
+                return result;
+            }
+            if ((a % 2 != 0) && (b % 2 == 0))
+            {
+                result = GetBiGCD(a, b / 2, out time);
+                st.Stop();
+                staineTime = st.ElapsedMilliseconds + time;
+                return result;
+            }
+            result = GetBiGCD(b, Math.Abs(a - b), out time);
+            st.Stop();
+            staineTime = st.ElapsedMilliseconds + time;
+            return result;
+        }
+
+
+        /// <summary>
+        /// Метод для построения гистограмм
+        /// </summary>
+        /// Данный метод сравнивает время нахождения решения каждым методом и
+        /// выдает их разность
+        /// <param name="euklidTime"></param>
+        /// <param name="staineTime"></param>
+        /// <returns>
+        /// если результат отрицательный значит Евклид отработал быстрее
+        /// если результат положительный значит Стейн отработал быстрее
+        /// </returns>
+        public long GetTimeDecision(long euklidTime,long staineTime)
+        {
+            long res = euklidTime - staineTime;
+            return res;
+        }
+    }
+}
--- EuklidesAlgorithmTests/EuklidesTests.cs
+++ EuklidesAlgorithmTests/EuklidesTests.cs
@@ -12,12 +12,66 @@ namespace Euklides.Tests
             // arrange
             int a = 48; // переменная которую будем передавать в метод Евклида
             int b = 36; // переменная которую будем передавать в метод Евклида
-            int expeted = 12; // ожидаемый результат 
+            int expected = 12; // ожидаемый результат 
             // action
             Euklides ea = new Euklides(); // создаем обьъект класса Euklides
             int actual = ea.GetGCD(a, b, out long time); // вызываем метод для рассчета алгоритма Евклида. Передаем наши переменный.
             // assert
-            Assert.AreEqual(expeted, actual); // производим сравнение ожидаемого результата с реальным результатом.
+            Assert.AreEqual(expected, actual); // производим сравнение ожидаемого результата с реальным результатом.
+        }
+
+        [TestMethod]
+        public void GetGCD_48and36and78_6resulted()
+        {
+            // arrange
+            int a = 48;
+            int b = 36;
+            int c = 78;
+            int expected = 6;
+
+            // action
+            Euklides ea = new Euklides(); 
+            int actual = ea.GetGCD(a, b, c);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetGCD_48and36and78and92_2resulted()
+        {
+            // arrange
+            int a = 48;
+            int b = 36;
+            int c = 78;
+            int d = 92;
+            int expected = 2;
+
+            // action
+            Euklides ea = new Euklides();
+            int actual = ea.GetGCD(a, b, c, d);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetGCD_34and36and38and40and42_2resulted()
+        {
+            // arrange
+            int a = 34;
+            int b = 36;
+            int c = 38;
+            int d = 40;
+            int e = 42;
+            int expected = 2;
+
+            // action
+            Euklides ea = new Euklides();
+            int actual = ea.GetGCD(a, b, c, d, e);
+
+            // assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
