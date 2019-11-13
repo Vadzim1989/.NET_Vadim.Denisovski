2611e68f954b3ba4a59d96c66b28401278e6a7fd
Вадим Денисовский <vadim.denisovski@live.com>
2019-11-14 00:18:25 +0300

    Создан класс Вектор

---
+++ Task_2/Vector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Vector
+    {
+        /// <summary>
+        /// координаты
+        /// </summary>
+        public int x, y, z;
+        /// <summary>
+        /// параметризованный конструктор
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        public Vector(int x = 0, int y = 0, int z = 0)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
