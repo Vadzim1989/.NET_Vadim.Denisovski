a03e3e8519d6f2fc429bcf3a8f7671dc4e20dfd0
Вадим Денисовский <vadim.denisovski@live.com>
2019-11-14 00:34:22 +0300

    перегрузка операторов +/-/*

--- Task_2/Vector.cs
+++ Task_2/Vector.cs
@@ -24,5 +24,54 @@ namespace Task_2
             this.y = y;
             this.z = z;
         }
+        /// <summary>
+        /// Перегружаем оператор сложения
+        /// </summary>
+        /// <param name="obj1"></param>
+        /// <param name="obj2"></param>
+        /// <returns>
+        /// Новый вектор со сложенными координатами
+        /// </returns>
+        public static Vector operator +(Vector obj1, Vector obj2)
+        {
+            Vector Vec = new Vector();
+            Vec.x = obj1.x + obj2.x;
+            Vec.y = obj1.y + obj2.y;
+            Vec.z = obj1.z + obj2.z;
+            return Vec;
+        }
+        /// <summary>
+        /// Перегружаем оператор разности
+        /// </summary>
+        /// <param name="obj1"></param>
+        /// <param name="obj2"></param>
+        /// <returns>
+        /// Новый вектор с разностью координат
+        /// </returns>
+        public static Vector operator -(Vector obj1, Vector obj2)
+        {
+            Vector Vec = new Vector();
+            Vec.x = obj1.x - obj2.x;
+            Vec.y = obj1.y - obj2.y;
+            Vec.z = obj1.z - obj2.z;
+            return Vec;
+        }
+        /// <summary>
+        /// Перегружаем оператор произведения
+        /// </summary>
+        /// <param name="obj1"></param>
+        /// <param name="obj2"></param>
+        /// <returns>
+        /// Новый вектор с произведением координат
+        /// </returns>
+        public static Vector operator *(Vector obj1, Vector obj2)
+        {
+            Vector Vec = new Vector();
+            Vec.x = obj1.x * obj2.x;
+            Vec.y = obj1.y * obj2.y;
+            Vec.z = obj1.z * obj2.z;
+            return Vec;
+        }
+        //...
     }
 }
