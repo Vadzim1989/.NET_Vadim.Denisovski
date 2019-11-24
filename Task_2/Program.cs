90c9b090983ee6bc9d6a7d630602449075b38991
Вадим Денисовский <vadim.denisovski@live.com>
2019-11-24 13:33:30 +0300

    готово

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -10,22 +10,6 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
-            Polynomial P1 = new Polynomial(1, 2);
-            var Calc = P1.Calculate(2);
-            Console.WriteLine(Calc);
-            Polynomial P2 = new Polynomial(10, 20, 30,40);
-            Polynomial P3 = P1 + P2;
-            Calc = P3.Calculate(2);
-            Console.WriteLine("*******************");
-            Console.WriteLine(Calc);
-            P3 = P1 - P2;
-            Calc = P3.Calculate(2);
-            Console.WriteLine("*******************");
-            Console.WriteLine(Calc);
-            P3 = P1 * P2;
-            Calc = P3.Calculate(2);
-            Console.WriteLine("*******************");
-            Console.WriteLine(Calc);
         }
     }
 }
