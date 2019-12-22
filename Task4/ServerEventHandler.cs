fcd125f7438cabd3e95b66364c10f670c69ddf9b
Вадим Денисовский <vadim.denisovski@live.com>
2019-12-23 00:27:21 +0300

    add ServerEventHandler

---
+++ Task4/ServerEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class ServerEventHandler
+    {
+        private Server Server { get; set; }
+
+        List<string> clientMsg = new List<string>();
+
+        public void EventHandler(Server server)
+        {
+            server.MessageFromClient += delegate (string msg)
+            {
+                clientMsg.Add(msg);
+            };
+        }
+
+        public ServerEventHandler(Server server)
+        {
+            Server = server;
+            EventHandler(server);
+        }
+    }
+}
